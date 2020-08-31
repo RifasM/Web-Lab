7150c4bdaabd8d2501c82ed07d88ffa8f9487fe2
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-31 15:11:54 +0530

    Refactor

--- resources/views/program2.blade.php
+++ resources/views/program2.blade.php
@@ -8,8 +8,7 @@
             <div class="col-md-11 m-b-md">
                 <div class="title content">Program 2</div>
                 <h3 class="content m-b-md">Aim: Write a JavaScript that calculates the squares and cubes of the numbers
-                    from 0 to 10
-                    and outputs HTML text that displays the resulting values in an HTML table format.</h3>
+                    from 0 to 10 and outputs HTML text that displays the resulting values in an HTML table format.</h3>
                 @include('identity')
             </div>
 
--- resources/views/program4.blade.php
+++ resources/views/program4.blade.php
@@ -7,9 +7,21 @@
         <div class="row flex-center m-b-md">
             <div class="col-md-11 m-b-md">
                 <div class="title content">Program 4</div>
-                <h3 class="content m-b-md">Aim: Write a JavaScript that calculates the squares and cubes of the numbers
-                    from 0 to 10
-                    and outputs HTML text that displays the resulting values in an HTML table format.</h3>
+                <h3 class="content m-b-md" style="text-align: left;">Aim: Develop and demonstrate a HTML5 file that
+                    includes JavaScript script that uses functions for the following problems:
+                    <ol type="a">
+                        <li>
+                            Parameter: A string
+                            <br>
+                            Output: The position in the string of the left-most vowel
+                        </li>
+                        <li>
+                            Parameter: A number
+                            <br>
+                            Output: The number with its digits in the reverse order
+                        </li>
+                    </ol>
+                </h3>
                 @include('identity')
             </div>
 
